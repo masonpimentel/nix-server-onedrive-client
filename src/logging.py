371e00902a50e903c1082b06e2717267827c9173
Manolito Mason Pimentel <mason@mpimentel.net>
2019-03-18 20:06:46 -0700

    Add logger

---
+++ src/logging.py
@@ -0,0 +1,9 @@
+from debugging import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("message")
+parser.add_argument("type")
+args = parser.parse_args()
+
+print_message(args.message, "CRONJOB", args.type)
