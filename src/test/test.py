aaea09792fbc75114c53ab6a79a9b93a10dda916
Manolito Mason Pimentel <mason@mpimentel.net>
2019-04-14 23:24:52 -0700

    Add parser for functional tests

--- src/test/test.py
+++ src/test/test.py
@@ -1,5 +1,22 @@
-import sys
+from lib.fs import *
 
-from lib.utils import *
+import argparse
+import tempfile
 
-print(config_get_user())
+# positional arguments:
+# num_from_dir  <integer>       number of directories that will be created on client
+# num_to_dir    <integer>       number of directories that will be created in OneDrive
+#
+
+parser = argparse.ArgumentParser()
+parser.add_argument("num_to_dir")
+args = parser.parse_args()
+
+with tempfile.TemporaryDirectory(dir="test") as tmpdirname:
+    print("Temp dir: " + tmpdirname)
+    for i in range(0, int(args.num_to_dir)):
+        print("Creating dir " + str(i))
+        os.mkdir(tmpdirname)
+
+
+
